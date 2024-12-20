aa2d57eb970a497728b4cc95b78bcd6dddc003ba
lnjihia <lillian1n2@hotmail.com>
2024-12-20 11:42:09 +0300

    Task 1.1

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@ principal = 1000
 rate = 0.05
 time = 3
 
-I = principal * rate * time
-print("The simple interest is:", I)
+interest = principal * rate * time
+print("The simple interest is:", interest)
