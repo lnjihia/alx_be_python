53b2cf8949f234cd708a5a856307d845f5e97bac
lnjihia <lillian1n2@hotmail.com>
2024-12-23 00:07:55 +0300

    Projects

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -19,9 +19,8 @@ match priority:
 
 # Modify the reminder if the task is time-bound
 if time_bound == "yes" and priority in {"high", "medium", "low"}:
-    reminder += " that requires immediate attention today!"
+    #reminder += " that requires immediate attention today!"
+    print(reminder + " that requires immediate attention today!")
 elif time_bound == "no" and priority in {"high", "medium", "low"}:
-    reminder += ". Consider completing it when you have free time."
-
-# Display the final reminder
-print("\n" + reminder)
+    #reminder += ". Consider completing it when you have free time."
+     print(reminder + ". Consider completing it when you have free time.")
